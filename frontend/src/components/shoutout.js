3badb067eb172fa4f2244122729d1f08290294f7
Tamuda <tchimhan@u.rochester.edu>
2024-04-12 01:21:48 -0400

    frontend skeleton working

--- frontend/src/components/shoutout.js
+++ frontend/src/components/shoutout.js
@@ -20,7 +20,7 @@ const deleteIcon = (
 
 const Shoutout = ({ name, title, text, deleteShout }) => {
   return (
-    <div className="flex text-sm md:text-[1rem] flex-col gap-4 justify-start text-stone-800 bg-stone-100  py-8 px-16 rounded-3xl md:w-2/3 w-full">
+    <div className="flex text-sm md:text-[1rem] flex-col gap-4 justify-start text-stone-800 bg-stone-100  py-8 md:px-16 px-8 rounded-3xl md:w-2/3 w-full">
       <div className="flex flex-row justify-between items-center">
         <Profile name={name} />
         <div
